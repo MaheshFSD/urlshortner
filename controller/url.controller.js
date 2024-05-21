7e5d26c8a54a029ead1a1edbde07a0170e80cc82
MaheshFSD <maheshfsdeveloper@gmail.com>
2024-05-21 16:11:27 +0530

    url redirection logic completed..

--- controller/url.controller.js
+++ controller/url.controller.js
@@ -5,18 +5,34 @@ const shortid = require('shortid');
 const createShortUrl = async (req, res) => {
     const redirectUrl = req.body.url;
     if(!redirectUrl) return res.status(400).json({message: 'Please provide Url.'});
+    const shortID = shortid();
     try {
         const urlObj = await Url.create({
-            shortId: shortid(),
+            shortId: shortID,
             redirectUrl,
             visitHistory: []
         })
-        res.status(201).json({message: 'short url created..'});
+        res.status(201).json({id: shortID});
     } catch (error) {
         res.status(500).json({message: error.message});
     }
 }
 
+const reditectUrl = async (req,res) => {
+    const shortId = req.params.id;
+    if(!shortId) return res.status(400).json({message: 'Please send valid url'});
+    // const rec = await Url.findOne({shortId});
+    const doc = await Url.findOneAndUpdate({shortId},{
+        $push: {
+            visitHistory: {
+                timeStamp: Date.now()
+            }
+        } 
+    }
+    );
+    res.redirect(doc.redirectUrl);
+    // console.log(rec, ' -------- rec ------ '); 
 
+}
 
-module.exports = {createShortUrl}
+module.exports = {createShortUrl, reditectUrl}
--- routes/url.route.js
+++ routes/url.route.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const {createShortUrl} = require('../controller/url.controller');
+const {createShortUrl, reditectUrl} = require('../controller/url.controller');
 
 router.post('/', createShortUrl);
-
+router.get('/:id', reditectUrl);
 module.exports = router;
